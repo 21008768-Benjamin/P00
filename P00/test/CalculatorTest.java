ad7efeb240d808a07cc546a3304a6c1263ccfd3d
21008768 <21008768@172.29.85.199>
2022-05-31 14:29:52 +0800

    first push to P00 repository in GitHub

---
+++ P00/test/CalculatorTest.java
@@ -0,0 +1,47 @@
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class CalculatorTest {
+	
+    int a;
+    int b;
+    Calculator cal;
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+		a = 1234;
+		b = 8765;
+		cal = new Calculator();
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		int actual = cal.add
+	}
+
+	@Test
+	public final void testAdd() {
+		int a = 1234;
+		int b = 8765;
+		
+		Calculator cal = new Calculator();
+		int actual = cal.add(a, b);
+		
+		int expected = 9999;
+		assertEquals (expected,actual);
+		
+	}
+
+}
